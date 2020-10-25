ac14266577612d43071540bf6858b85fda1b56f0
zhangshuo <zhangshuocool@163.com>
2020-10-25 23:25:35 +0800

    add lm file load.

--- src/coder3Gun/Gun3.java
+++ src/coder3Gun/Gun3.java
@@ -1,8 +1,24 @@
 package coder3Gun;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Gun3 {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static float[] charLM = new float[27 * 27 * 27 * 27];
+
+    public static void main(String[] args) throws IOException {
+
+        byte[] bytes = Files.readAllBytes(Path.of(args[0]));
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer().get(charLM);
+        byteBuffer.clear();
+
+        System.out.println("completed");
     }
 }
