66e1d350f51d0a8156575f24203737a5b58d31ae
zhangshuo <zhangshuocool@163.com>
2020-10-26 00:37:24 +0800

    add lm file load.

--- src/coder3Gun/Gun3.java
+++ src/coder3Gun/Gun3.java
@@ -1,6 +1,9 @@
 package coder3Gun;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.file.Files;
@@ -9,7 +12,8 @@ import java.nio.file.Paths;
 
 public class Gun3 {
 
-    private static float[] charLM = new float[27 * 27 * 27 * 27];
+    static float[] charLM = new float[27 * 27 * 27 * 27];
+    static int LINEBUFSIZE = 1024;
 
     public static void main(String[] args) throws IOException {
 
@@ -19,6 +23,30 @@ public class Gun3 {
         byteBuffer.order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer().get(charLM);
         byteBuffer.clear();
 
+        bytes = null; // for gc
+
+        byte[] buffer = new byte[LINEBUFSIZE * 2];
+        short[] lineBuffer = new short[LINEBUFSIZE];
+
+        int bytesRead;
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        int c = 0;
+//        while ((c = reader.read()) >= 0) {
+//            if (c == 0)
+//            {
+//                System.out.println(c);
+//            }
+//        }
+
+        while ((bytesRead = System.in.read(buffer)) > 0) {
+            byteBuffer = ByteBuffer.wrap(buffer);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(lineBuffer);
+            byteBuffer.clear();
+        }
+
         System.out.println("completed");
+
+
     }
 }
